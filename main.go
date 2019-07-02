c2090d9b8d6d9ab5c353a92526fffbba3a5fd07a
Concourse CI <git_concourse_ci@comcast.com>
2019-07-02 23:03:37 +0000

    [skip ci] bump version from 0.4.0 to 0.5.0

--- main.go
+++ main.go
@@ -45,7 +45,7 @@ import (
 const (
 	applicationName, apiBase = "fenrir", "/api/v1"
 	DEFAULT_KEY_ID           = "current"
-	applicationVersion       = "0.4.0"
+	applicationVersion       = "0.5.0"
 )
 
 type FenrirConfig struct {
